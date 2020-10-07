bc5cc9e6c18ea92163a73e11fa6891e3613d1658
Joshua Novikoff <joshnovikoff@my.cwi.edu>
2020-10-07 16:25:50 -0600

    Added code to move between levels depending on Alive, Trancending, Dying.

--- 3_Boost/Assets/Rocket.cs
+++ 3_Boost/Assets/Rocket.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 250f;
+    enum State { Alive, Dying, Trancending };
+    State state = State.Alive;
     Rigidbody rigidBody;
     AudioSource audioSource;
     // Start is called before the first frame update
@@ -18,8 +19,11 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if ( state == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }        
     }
     
     private void Rotate()
@@ -56,21 +60,35 @@ public class Rocket : MonoBehaviour
     }
     void OnCollisionEnter(Collision collision)
     {
+        if(state != State.Alive) { return; }
         switch (collision.gameObject.tag)
         {
             case "Friendly":
                 // do nothing, someone I like
-                print("Friendly");
                 break;
-            case "Fuel":
-                // My spaceship is hungry
-                print("Fuel");
+            case "Finish":
+                // Go to next level                
+                state = State.Trancending;
+                Invoke("LoadNextLevel", 3f);
                 break;
             default:
                 // die
-                print("Die!");
+                state = State.Dying;
+                Invoke("LoadFirstLevel", 3f);
                 break;
         }
 
+    }
+
+    private void LoadFirstLevel()
+    {
+        state = State.Alive;
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel()
+    {
+        state = State.Alive;
+        SceneManager.LoadScene(1);
     }
 }

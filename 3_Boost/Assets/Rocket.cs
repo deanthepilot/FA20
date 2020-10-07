55afae5c4276261098cc7a4604b782297e2fcda0
Joshua Novikoff <joshnovikoff@my.cwi.edu>
2020-10-07 14:14:00 -0600

    Completed rigidBody rotations and added thrust audio.

--- 3_Boost/Assets/Rocket.cs
+++ 3_Boost/Assets/Rocket.cs
@@ -7,33 +7,49 @@ public class Rocket : MonoBehaviour
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
     Rigidbody rigidBody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
+    }
+    
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true;
+        if (Input.GetKey(KeyCode.A))
+        {            
+            transform.Rotate(Vector3.forward);            
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {            
+            transform.Rotate(-Vector3.forward);            
+        }
+        rigidBody.freezeRotation = false;
     }
 
-    private void ProcessInput()
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
         }
-
-        if(Input.GetKey(KeyCode.A))
+        else
         {
-            transform.Rotate(Vector3.forward);
-        }
-
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-Vector3.forward);
+            audioSource.Stop();
         }
     }
 }

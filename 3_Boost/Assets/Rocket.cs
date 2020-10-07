2e810b8a9db90ca32e10bbc109e1df5752a4ec2f
Joshua Novikoff <joshnovikoff@my.cwi.edu>
2020-10-07 15:02:39 -0600

    Added tags for Friendly, Fuel. Added code to support tags during collission

--- 3_Boost/Assets/Rocket.cs
+++ 3_Boost/Assets/Rocket.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     [SerializeField] float rcsThrust = 100f;
-    [SerializeField] float mainThrust = 100f;
+    [SerializeField] float mainThrust = 250f;
     Rigidbody rigidBody;
     AudioSource audioSource;
     // Start is called before the first frame update
@@ -24,24 +24,26 @@ public class Rocket : MonoBehaviour
     
     private void Rotate()
     {
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
         rigidBody.freezeRotation = true;
         if (Input.GetKey(KeyCode.A))
         {            
-            transform.Rotate(Vector3.forward);            
+            transform.Rotate(Vector3.forward * rotationThisFrame);            
         }
 
         else if (Input.GetKey(KeyCode.D))
         {            
-            transform.Rotate(-Vector3.forward);            
+            transform.Rotate(-Vector3.forward * rotationThisFrame);            
         }
         rigidBody.freezeRotation = false;
     }
 
     private void Thrust()
     {
+        float thrustThisFrame = mainThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
@@ -51,5 +53,24 @@ public class Rocket : MonoBehaviour
         {
             audioSource.Stop();
         }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                // do nothing, someone I like
+                print("Friendly");
+                break;
+            case "Fuel":
+                // My spaceship is hungry
+                print("Fuel");
+                break;
+            default:
+                // die
+                print("Die!");
+                break;
+        }
+
     }
 }

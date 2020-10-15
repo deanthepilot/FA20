8b4bb1b89c9b0ac1d992936be7037a09ec914586
Joshua Novikoff <joshnovikoff@my.cwi.edu>
2020-10-15 13:33:05 -0600

    Refactored asset packs, added SceneLoader

--- 4_Argon_Assault/Assets/Scripts/MusicPlayer.cs
+++ 4_Argon_Assault/Assets/Scripts/MusicPlayer.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.SceneManagement;
-using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour
 {
@@ -10,11 +8,5 @@ public class MusicPlayer : MonoBehaviour
     void Start()
     {
         DontDestroyOnLoad(gameObject);
-        Invoke("LoadFirstScene", 5);
-    }
-
-    void LoadFirstScene()
-    {
-        SceneManager.LoadScene(1);
-    }
+    }    
 }

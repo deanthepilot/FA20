a6a5e1a166108c473e0fab75822b42dfe69a8214
Joshua Novikoff <joshnovikoff@my.cwi.edu>
2020-10-23 12:05:11 -0600

    Finalized Player Movement, Added Box Collider to Spikes

--- 5_Side_Scroller/Assets/Scripts/PlayerMovement.cs
+++ 5_Side_Scroller/Assets/Scripts/PlayerMovement.cs
@@ -1,23 +1,52 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float jumpForce = 1000f;
+    public float jumpForce = 1500f;
     Rigidbody rb;
+    public bool isGrounded;
+    public float speed = 10;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
 
-    
+    void OnCollisionExit()
+    {
+        isGrounded = false;
+    }
     void Update()
     {
+        PlayerJump();
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.transform.Translate(Vector3.left * Time.deltaTime * speed, Space.Self); //LEFT
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.transform.Translate(Vector3.right * Time.deltaTime * speed, Space.Self); //RIGHT
+        }
+    }
+
+    void PlayerJump()
+    {
         float jumpFrame = jumpForce * Time.deltaTime;
-        if (Input.GetKey(KeyCode.Space))
-        {            
-            rb.AddForce(Vector3.up * jumpFrame);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpFrame, ForceMode.Impulse);
+            isGrounded = false;
         }
     }
 }
